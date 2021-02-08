868a117ef9d2f6e7d9cabf6e6c4a813aabe39f1b
Clarence Castillo <hello@clarencecastillo.me>
2021-02-08 15:35:38 +0800

    feat(lib): add option to control starting drag select over items (#117)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,6 +19,7 @@ export class AppComponent implements OnInit {
   disableRangeSelection = false;
   isDesktop = false;
   selectWithShortcut = false;
+  dragOverItems = true;
 
   constructor(
     private titleService: Title,
